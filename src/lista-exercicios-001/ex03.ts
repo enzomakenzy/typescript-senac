01ab3374539e7fb9f8a36ae5d4a56fe3014886d2
Enzo Makenzy <143420071+enzomakenzy@users.noreply.github.com>
2025-06-22 15:33:58 -0300

    Exercício 3 da Lista de Exercícios 001 finalizado

---
+++ src/lista-exercicios-001/ex03.ts
@@ -0,0 +1,23 @@
+let number1: number = 4;
+let number2: number = 2;
+
+let soma: number = number1 + number2;
+let multiplicacao: number = number1 * number2;
+
+console.log(soma, multiplicacao);
+
+let primeiroNome: string = "Enzo";
+let segundoNome: string = "Makenzy";
+
+let nomeSocial: string = primeiroNome + " " + segundoNome; 
+
+console.log(nomeSocial);
+
+let estaMatriculado: boolean = false;
+
+if (estaMatriculado) {
+  console.log("Não é um aluno");
+}
+
+let valorIndefinido: undefined = undefined;
+console.log(valorIndefinido);
