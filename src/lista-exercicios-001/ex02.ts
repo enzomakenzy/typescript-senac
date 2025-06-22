92a405d545cbb52f013fec9e2e83eb69313f529e
Enzo Makenzy <143420071+enzomakenzy@users.noreply.github.com>
2025-06-22 15:12:41 -0300

    Exercício 2 da Lista de Exercícios 001 finalizado

---
+++ src/lista-exercicios-001/ex02.ts
@@ -0,0 +1,8 @@
+// As variáveis declaradas com "let" só existem dentro do bloco que foram declaradas e permitem reatribuição de valor
+let user: string = "Enzo";
+
+// As constantes, que são declaradas com "const", só existem dentro do seu bloco e NÃO PERMITEM reatribuição de valor
+const id: number = 1;
+
+// As variáveis declaradas com "var" possuem um escopo global, e permitem reatribuição.
+var instrumento: string = "Violão";
